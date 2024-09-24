5a8bcd03c303cc49e5a7a08de7598091b31e47d9
Mason <wen0093@flinders.edu.au>
2024-09-24 13:05:20 +0930

    structure clearfication

---
+++ OpenShop-X.Models/ResponseData.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenShop_X.Models
+{
+    public class ResponseData
+    {
+        public Object? Result { get; set; }
+
+        public bool IsSuccessful { get; set; }
+
+        public string Msg { get; set; } = string.Empty;
+
+        public int? ErrorCode { get; set; }
+
+        public string? ErrorMsg { get; set; } = string.Empty;
+    }
+}
